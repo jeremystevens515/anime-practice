6e6cea7edef9d53a5b2c96fa11bb132690dd64d8
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-07-20 10:25:03 -0400

    added a pentagon to animate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+      <svg>
+        <polygon points="
+        150 5,
+        225 60,
+        200 145,
+        100 145,
+        75 60" 
+        stroke="black" fill="none" stroke-width="2" />
+      </svg>
+  );
+}
