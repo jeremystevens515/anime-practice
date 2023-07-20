6ca3a1cc186e34d57d16e2ea7b8e6a2ed3a4776b
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-07-20 10:39:13 -0400

    added first animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import anime from "animejs";
+
 export default function App() {
+  // anime({
+  //   targets: ".shape",
+  //   points: [
+  //     {value: "175 5, 225 60, 200 145, 100 145, 75 60"}
+  //   ],
+  //   easing: 'easeInOutQuad',
+  //   duration: 1000,
+  //   loop: false,
+  //   direction: 'alternate'
+  // })
+
   return (
-      <svg>
-        <polygon points="
-        150 5,
-        225 60,
-        200 145,
-        100 145,
-        75 60" 
+      <svg >
+        <polygon className="shape" points="150 5, 225 60, 200 145, 100 145, 75 60" 
         stroke="black" fill="none" stroke-width="2" />
       </svg>
   );
